bb894ce39abffffce3bfb2a60be75285e88374ab
Sami J. Mäkinen <sjm@iki.fi>
2026-04-26 05:18:32 +0300

    incomplete

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -6,34 +6,58 @@ fn area(p1: (i64, i64), p2: (i64, i64)) -> i64 {
     (1 + (p1.0 - p2.0).abs()) * (1 + (p1.1 - p2.1).abs())
 }
 
+fn p_min(p1: (i64, i64), p2: (i64, i64)) -> (i64, i64) {
+    (p1.0.min(p2.0), p1.1.min(p2.1))
+}
+fn p_max(p1: (i64, i64), p2: (i64, i64)) -> (i64, i64) {
+    (p1.0.max(p2.0), p1.1.max(p2.1))
+}
+
 fn main() -> anyhow::Result<()> {
     let mut opts = OptsCommon::parse();
     opts.start_pgm(env!("CARGO_BIN_NAME"))?;
 
-    let mut tiles_vec = Vec::new();
+    let mut pos_vec = Vec::new();
     for (n, line) in io::stdin().lock().lines().enumerate() {
         let line = line?;
         let coords = line.split(',').collect::<Vec<_>>();
         if coords.len() != 2 {
             bail!("Invalid line #{n}: {line:?}");
         }
-        tiles_vec.push((coords[0].parse::<i64>()?, coords[1].parse::<i64>()?));
+        pos_vec.push((coords[0].parse::<i64>()?, coords[1].parse::<i64>()?));
     }
-    let n_tiles = tiles_vec.len();
-    debug!("Read tiles positions ({n_tiles}): {tiles_vec:?}");
+    let n_pos = pos_vec.len();
+    info!("Read tiles positions: {n_pos}");
+    debug!("{pos_vec:?}");
 
-    let mut pair_vec = Vec::with_capacity(n_tiles * n_tiles); // a bit extra
-    for i in 0..n_tiles {
-        for j in i + 1..n_tiles {
-            pair_vec.push((&tiles_vec[i], &tiles_vec[j]));
+    let mut pairs = Vec::with_capacity(n_pos * n_pos);
+    let mut areas = Vec::with_capacity(n_pos * n_pos);
+    for i in 0..n_pos {
+        for j in i + 1..n_pos {
+            pairs.push((pos_vec[i], pos_vec[j]));
+            areas.push((
+                p_min(pos_vec[i], pos_vec[j]),
+                p_max(pos_vec[i], pos_vec[j]),
+            ));
         }
     }
-    debug!("Combinations ({}):\n{:?}", pair_vec.len(), pair_vec);
+    info!("Combinations: {}", pairs.len());
+    debug!("{:?}", pairs);
 
     // sort by area
-    pair_vec.sort_by(|p1, p2| area(*p2.0, *p2.1).cmp(&area(*p1.0, *p1.1)));
-    let winner = pair_vec[0];
-    println!("Part 1: {}", area(*winner.0, *winner.1));
+    pairs.sort_by(|p1, p2| area(p2.0, p2.1).cmp(&area(p1.0, p1.1)));
+    let winner = pairs[0];
+    println!("Part 1: {}", area(winner.0, winner.1));
+
+    areas.sort_by(|p1, p2| area(p2.0, p2.1).cmp(&area(p1.0, p1.1)));
+    let mut edges = Vec::with_capacity(n_pos);
+    pos_vec.iter().skip(1).for_each(|p1| {
+        edges.push(((pos_vec[0].0, pos_vec[0].1), (p1.0, p1.1)))
+    });
+    edges.push((
+        (pos_vec[n_pos - 1].0, pos_vec[n_pos - 1].1),
+        (pos_vec[0].0, pos_vec[0].1),
+    ));
     Ok(())
 }
 // EOF
